c6fe6e0bfc958cff173d5a6b981f4fc6aa61c792
pylarao <108193838+pylarao@users.noreply.github.com>
2022-06-25 23:46:20 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 var config = {
-    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"127.0.0.1",
-    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"database",
+    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"testing.cco34hgoktkv.ap-south-1.rds.amazonaws.com",
+    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"testing",
     DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"user",
-    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"pass",
+    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"pass1234",
 }
 
 module.exports = config;
